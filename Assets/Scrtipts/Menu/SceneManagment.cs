56c574bdc9cca464e269dec5ca4fa6f674027ae9
Yrd Dany <bierch322@mail.ru>
2022-01-22 19:14:09 +0300

    Main commit
    22.01.2022. basic assembly

---
+++ Assets/Scrtipts/Menu/SceneManagment.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagment : MonoBehaviour
+{
+    public void Scenes(int numberScenes)
+    {
+        SceneManager.LoadScene(numberScenes);
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
