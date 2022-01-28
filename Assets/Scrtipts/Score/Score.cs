0ba8e879c5e8b2f9e1404f50363cc4bd3191bcca
Yrd Dany <bierch322@mail.ru>
2022-01-28 21:57:42 +0300

    Commit
    test commit TLO

---
+++ Assets/Scrtipts/Score/Score.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Score : MonoBehaviour
+{
+    public static GameController instance; 
+    public int _score, HightScore;
+    public Text ScoreText, HightScoreText;
+
+    void Awake()
+    {
+        instance = this;
+        if(PlayerPrefs.HasKey("SaveScore"))
+        {
+        HightScore = PlayerPrefs.GetInt("SaveScore");
+        }
+    }
+    void Update()
+    {
+        ScoreText.text = ((int)_score).ToString();
+        HightScoreText.text = ((int)HightScore).ToString();
+    }
+     
+     private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            instance.AddScore();
+            //AddScore();
+            HightScore ++;
+            Destroy(other.gameObject);
+        }
+    }
+    public void AddScore()
+    {
+        _score ++;
+        AddHightScore();
+    }
+    public void AddHightScore()
+    {
+        if(_score> HightScore)
+        {
+            HightScore= _score;
+            PlayerPrefs.SetInt("SaveScore", HightScore);        
+        }
+    }
+    public void ResetScore()
+    {
+        _score = 0;
+    }
+}
